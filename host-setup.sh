37feabdb7a12525517a4ac611ceee44ffd9b54a0
Cloud Cafe <68260844+cloudcafetech@users.noreply.github.com>
2020-08-09 16:38:21 +0530

    Update host-setup.sh

--- host-setup.sh
+++ host-setup.sh
@@ -232,7 +232,29 @@ wget https://raw.githubusercontent.com/cloudcafetech/kube-katakoda/master/setup-
 chmod +x setup-helm.sh
 ./setup-helm.sh
 
-# Setup Ecosystem
+# Setup for monitring and logging
+exit
+wget https://raw.githubusercontent.com/cloudcafetech/kube-katakoda/master/kubemon.yaml
+wget https://raw.githubusercontent.com/cloudcafetech/kube-katakoda/master/kubelog.yaml
+wget https://raw.githubusercontent.com/cloudcafetech/kube-katakoda/master/loki.yaml
+wget https://raw.githubusercontent.com/cloudcafetech/kube-katakoda/master/loki-ds.json
+wget https://raw.githubusercontent.com/cloudcafetech/kube-katakoda/master/pod-monitoring.json
+wget https://raw.githubusercontent.com/cloudcafetech/kube-katakoda/master/kube-monitoring-overview.json
+wget https://raw.githubusercontent.com/cloudcafetech/kube-katakoda/master/cluster-cost.json
 
+kubectl create ns monitoring
+kubectl create -f kubemon.yaml -n monitoring
+kubectl create ns logging
+kubectl create secret generic loki -n logging --from-file=loki.yaml
+kubectl create -f kubelog.yaml -n logging
+
+HIP=`ip -o -4 addr list eth0 | awk '{print $4}' | cut -d/ -f1`
+curl -vvv http://admin:admin2675@$HIP:30000/api/dashboards/db -X POST -d @pod-monitoring.json -H 'Content-Type: application/json'
+curl -vvv http://admin:admin2675@$HIP:30000/api/dashboards/db -X POST -d @kube-monitoring-overview.json -H 'Content-Type: application/json'
+curl -vvv http://admin:admin2675@$HIP:30000/api/dashboards/db -X POST -d @cluster-cost.json -H 'Content-Type: application/json'
+curl -vvv http://admin:admin2675@$HIP:30000/api/datasources -X POST -d @loki-ds.json -H 'Content-Type: application/json' 
 
 # Setup Demo application
+exit
+wget https://raw.githubusercontent.com/cloudcafetech/kube-katakoda/master/mongo-employee.yaml
+kubectl create -f mongo-employee.yaml
